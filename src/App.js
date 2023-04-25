4c43e2cd4e2071d76c2e9c9a00f43766e3e93229
Brett Cantrell <cantrell.brett8892@gmail.com>
2023-04-24 21:11:52 -0500

    Created SearchBar + Updated MusicTable + Imported all components into App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import AddNewSong from './Components/AddNewSong';
 import SearchBar from './Components/SearchBar/SearchBar';
 import './App.css';
+import axios from 'axios';
 
 
 
 function App() {
-  const [songs, setSongs] = useState([])
+  const [songs, setSongs] = useState([]);
 
-  function addNewSong(song){
-    let tempSongs = [...songs, song];
+  useEffect(() => {
+    
+    getAllSongs();
+  }, []);
 
-    setSongs(tempSongs);
-  }
-
+  
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data)
+    setSongs(response.data)
+  };
+  async function addNewSong(newSong){
+    let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
+    console.log(response.data);
+    if(response.status === 201){
+      await getAllSongs();
+    }
+  };
+  async function deleteSong(song){
+    let response = await axios.delete('http://127.0.0.1:8000/api/music/', song);
+    if(response.status === 200){
+      return getAllSongs();
+    }
+  };
+  
   
   return (
     <div>
-      <NavBar />
-      <SearchBar searchedSongProperty={songs} />
-      <AddNewSong addNewSongProperty={addNewSong}/>
+      <div><NavBar/></div>
+      <div><SearchBar displaySong = {songs} deleteSongProp = {deleteSong}/></div>
+      <div className='content-wrap'><AddNewSong addNewSongProperty={addNewSong}/></div>
+      
     </div>
     )
   }
   
   
-  
-  
   export default App;
   
-  // async function getAllSongs(){
-  //   const response = await axios.get('http://127.0.0.1:8000/api/music/');
-  //   console.log(response.data)
-  //   setSongs(response.data)
-  // };
-
-
-  // useEffect(() => {
-  //     getAllSongs();
-  // }, []);
   
   
-  {/* to changes songs in table need to change songs in musictable, need to use props unless songs changes to get songs to changes need to 
-  setsongs assigns songs new value, songs var has to change only useeffect is in here. filtering only in app.js, pay attention to props and addnewentry
-  concepts in general,  */}
+  // I don't know if I can have setSongs in both the post and get requests 
+// addnewsong goes on button click when form gets submitted
+// calling addnewsong without a song to add
+  //  to changes songs in table need to change songs in musictable, need to use props unless songs changes 
+  // to get songs to changes need to 
+  // setsongs assigns songs new value, songs var has to change only useeffect is in here. filtering only 
+  // in app.js, pay attention to props and addnewentry
+  // concepts in general,  
      
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,29 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
 
-const SearchBar = () => {
-    const [songSearch, setSongSearch] = useState('');
 
-    async function searchForSong(event) {
-        event.preventDefault();
-        const response = await axios.get('http://127.0.0.1:8000/api/music/'+ songSearch);
-        setSongSearch(response.data)
-    };
-    useEffect(() => {
-        searchForSong();
-    });
 
+const SearchBar = (props) => {
+
+    const [searchSong, setSearchSong] = useState('');
 
     return ( 
-        <form onSubmit={searchForSong}>
-            <div className='container'>
-                <h4>Search Music Library</h4>
-                <input type='text' onChange={(event) => setSongSearch(event.target.value)}/>
-                <button onClick={(event) => searchForSong(event.target.value)}>Search</button>
+        <><div className="display-container">
+            <div className="table-title">
+                <h2 className="library-title">Song List</h2>
             </div>
-        </form>
+            <div className="search-filter">
+                <label className="search-label">Search Song List:</label>
+                <input type='text' className='custom-input' placeholder='Search...' onChange={(event) => setSearchSong(event.target.value)} />
+            </div>
+        </div><table className="song-table">
+                <thead>
+                    <tr className="header-row">
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                        <th>Release Date</th>
+                        <th>Genre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.displaySongs.filter((song) => {
+                        if(searchSong == ''){
+                            return song;
+                        } else if (song.title.toLowerCase().includes(searchSong.toLowerCase())){
+                            return song;
+                        }
+                    })
+                    .map((song, index) => {
+                        return(
+                            <tr key={index} className='display-items'>
+                                <td>{song.title}</td>
+                                <td>{song.artist}</td>
+                                <td>{song.album}</td>
+                                <td>{song.release_date}</td>
+                                <td>{song.genre}</td>
+                                <td className='button-conatin'><button type='submit' className='delete-button' onClick={() => props.deleteSongProp(song)}>Delete</button></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table></>
      );
 }
  
 export default SearchBar;
+// do not need axios call
+// needs function that filters the data set i already have(ie songs)
+
+// only want one input and start with getting the titles to work then add additional pieces 
+// look at .includes from most wanted
+
+
